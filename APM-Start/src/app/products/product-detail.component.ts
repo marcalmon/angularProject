e98ddd224551cea952ad054cc4df22cd9a3b00f8
Mar <marical2222@gmail.com>
2018-06-12 18:55:27 -0300

    ABGuards Service

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
 
@@ -12,7 +12,8 @@ export class ProductDetailComponent implements OnInit {
     pageTitle: string = 'Product Detail'
     product: IProduct;
 
-  constructor(private _route: ActivatedRoute) { }
+  constructor(private _route: ActivatedRoute,
+              private _router: Router) { }
 
   ngOnInit() {
     let id = +this._route.snapshot.paramMap.get('id');
@@ -27,6 +28,10 @@ export class ProductDetailComponent implements OnInit {
       "starRating": 3.2,
       "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
     }
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
   }
 
 }
